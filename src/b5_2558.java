a7ff755dc30729878a80b56fb79d439bf0208e5f
osh <okzzangsh@gmail.com>
2022-04-07 19:26:27 +0900

    bronze5

---
+++ src/b5_2558.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class b5_2558 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a+b);
+	}
+}
