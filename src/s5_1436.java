a5db2ff807b21959e76e21c440830da3b23fd301
osh <okzzangsh@gmail.com>
2022-05-09 23:46:33 +0900

    silver5_1436

---
+++ src/s5_1436.java
@@ -0,0 +1,19 @@
+import java.util.*;
+ 
+public class s5_1436 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+ 
+		int num = 666;
+		int count = 1;
+        
+		while(count != N) {
+			num++;
+			if(String.valueOf(num).contains("666")) {
+				count++;
+			}
+		}
+		System.out.println(num);
+	}
+}
